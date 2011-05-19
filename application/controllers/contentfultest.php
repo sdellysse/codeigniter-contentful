b18f3cfb4d84485171f9e5f85ad94f34f520798e
Shawn Dellysse <shawn@noinc.com>
2011-05-19 14:25:13 -0400

    converted to spark format

--- application/controllers/contentfultest.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ContentfulTest extends CI_Controller {
-    public function index () {
-        $this->load->library('Contentful');
-
-        $data['name'] = 'Shawn Dellysse';
-        $this->contentful->load('contentful_test/index', $data);
-    }
-}
---
+++ config/autoload.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') or die('No direct script access allowed');
+
+$autoload['config'] = array('contentful');
+$autoload['helper'] = array('contentfulmanager');
+$autoload['library'] = array('contentfulmanager', 'contentful');
